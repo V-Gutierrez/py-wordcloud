a3096ca10d65c4342d50bb8eecf445d9494e1164
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-07-25 20:44:39 -0300

    chore: add stopwords

--- word_cloud.py
+++ word_cloud.py
@@ -3,17 +3,23 @@
 # importing all necessary modules
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import nltk
+
+nltk.download('stopwords')
+
+stopwords = nltk.corpus.stopwords.words('portuguese')
 
 words = []
 
 with open('db.txt', 'r', encoding='utf-8') as File:
     for line in File:
-        if len(line) > 3:
+        if len(line) > 3:  # Dummy stopwords filter
             words.append(str(line))
 
 wordcloud = WordCloud(
     width=400,
     height=400,
+    stopwords=stopwords,
     background_color='black',
     min_font_size=10).generate(' '.join([item for item in words]))
 
